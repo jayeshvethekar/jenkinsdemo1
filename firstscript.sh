5e6d657d6a47103f43b9e342ac62a834287fbc3f
root <root@ip-172-31-28-148.ec2.internal>
2019-10-09 12:23:25 +0000

    first from master

--- firstscript.sh
+++ firstscript.sh
@@ -1,6 +1,6 @@
 echo " this is my first script"
 echo " we are adding two numbers"
-echo "added a line"
+echo "added a line 3"
 echo " added second line"
 num=${numberfirst}+${numbersecond}
 echo $num
